b34a89b471e1bbe74299c0a13d96e9691d462212
Ubuntu <ubuntu@ip-10-250-80-246.us-west-2.compute.internal>
2013-07-13 21:59:52 +0000

    Added read from html file.

--- web.js
+++ web.js
@@ -1,9 +1,12 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.writeHead(200, {'Content-Type': 'text/html'});  
+  var fileContents = fs.readFileSync('index.html');
+  response.send(fileContents);
 });
 
 var port = process.env.PORT || 5000;
