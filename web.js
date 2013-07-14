041b2c62252c9fb7ccd2b4f1dd2d82c8463555d2
Ubuntu <ubuntu@ip-10-250-80-246.us-west-2.compute.internal>
2013-07-14 03:13:59 +0000

    Go back to working state with dumb print.

--- web.js
+++ web.js
@@ -5,9 +5,10 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
   response.writeHead(200, {'Content-Type': 'text/html'});  
-  var fileContents = fs.readFileSync('index.html');
+//  var fileContents = fs.readFileSync('index.html');
 //  console.log(fileContents.toString('utf-8'));
-  response.send(fileContents.toString('utf-8'));
+//  response.send(fileContents.toString('utf-8'));
+    response.send("I am OK");
 });
 
 
