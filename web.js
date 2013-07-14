bee108663f37a256e94aab4f07c2d7c8eaba0481
Ubuntu <ubuntu@ip-10-250-80-246.us-west-2.compute.internal>
2013-07-14 03:37:36 +0000

    Output set to contens of index.html.

--- web.js
+++ web.js
@@ -4,11 +4,8 @@ var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-//  response.writeHead(200, {'Content-Type': 'text/html'});  
   var fileContents = fs.readFileSync('index.html');
-//  console.log(fileContents.toString('utf-8'));
-//  response.send(fileContents.toString('utf-8'));
-    response.send("I am OK");
+  response.send(fileContents.toString('utf-8'));
 });
 
 
