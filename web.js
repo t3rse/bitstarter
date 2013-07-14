f676c9baf767451a85ff1975660984638320fe4a
Ubuntu <ubuntu@ip-10-250-80-246.us-west-2.compute.internal>
2013-07-14 02:06:47 +0000

    Use buffer tostring with utf-8 encoding.

--- web.js
+++ web.js
@@ -6,10 +6,13 @@ var app = express.createServer(express.logger());
 app.get('/', function(request, response) {
   response.writeHead(200, {'Content-Type': 'text/html'});  
   var fileContents = fs.readFileSync('index.html');
-  response.send(fileContents);
+//  console.log(fileContents.toString('utf-8'));
+  response.send(fileContents.toString('utf-8'));
 });
+
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
